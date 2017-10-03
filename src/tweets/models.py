58f60135aaa84c5d32feb7c6c2e259fc17db6fc1
Younes Henni <henni1989younes@gmail.com>
2017-10-03 21:55:30 +0100

    Create a tweet app and a model to connect it to DB

---
+++ src/tweets/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+class Tweet(models.Model):
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content     = models.CharField(max_length=255)
+    updated     = models.DateTimeField(auto_now=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
