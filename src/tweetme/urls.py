df7f6428161e8de5773ef980e535646276e69d63
Younes Henni <henni1989younes@gmail.com>
2017-10-04 17:45:18 +0100

    Adding media object, navbar, and columns/rows from bootstrap

--- src/tweetme/urls.py
+++ src/tweetme/urls.py
@@ -19,8 +19,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import home
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
     url(r'^tweets/', include('tweets.urls', namespace='tweets')),
 ]
 
