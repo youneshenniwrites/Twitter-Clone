09dd00501a5f14fa12da69cca83b56a2d057ded4
Younes Henni <henni1989younes@gmail.com>
2017-10-05 20:06:47 +0100

    adding a new app for account management including the rest api account

---
+++ src/accounts/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserDisplaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+        ]
--- src/tweets/api/serializers.py
+++ src/tweets/api/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from tweets.models import Tweet
+from accounts.api.serializers import UserDisplaySerializer
 
 
 class TweetModelSerializer(serializers.ModelSerializer):
+    user = UserDisplaySerializer()
     class Meta:
         model = Tweet
         fields = [
